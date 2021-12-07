47cabeff1606a29a59e05c1b1a03064a93fc20b8
Luis Holanda <luiscmholanda@gmail.com>
2021-12-07 19:37:02 -0300

    feat: add htop module

---
+++ modules/programs/shell/htop.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+  config.user.home.programs.htop = {
+    enable = true;
+    settings = {
+      cpu_count_from_zero = true;
+      delay = 5;
+      detailed_cpu_time = true;
+      highlight_base_name = true;
+      shadow_other_users = true;
+      show_cpu_frequency = true;
+    };
+  };
+}
