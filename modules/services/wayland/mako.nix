dea8ea1fbe1aabb510c56e12f6a9aca8a5cd7418
Luis Holanda <luiscmholanda@gmail.com>
2021-12-07 20:23:03 -0300

    feat: finish mako configuration

--- modules/services/wayland/mako.nix
+++ modules/services/wayland/mako.nix
@@ -1,8 +1,27 @@
 { config, lib, pkgs, ... }:
 let
+  inherit (lib) mkIf;
   inherit (lib.my) mkEnableOpt;
+
+  colors = config.theme.colors;
+  uiFont = builtins.head config.fonts.fontconfig.defaultFonts.sansSerif;
+  uiSize = config.theme.fonts.size.ui;
 in {
   options.modules.services.mako = {
     enable = mkEnableOpt "Enable mako notification daemon.";
+  };
+
+  config.user.home.programs.mako = mkIf config.modules.service.mako.enable {
+    enable = true;
+    maxVisible = 3;
+    layer = "overlay";
+    font = "${uiFont} ${builtins.toString uiSize}";
+    backgroundColor = colors.background.hex;
+    textColor = colors.foreground.hex;
+    # TODO: These should be moved to a theme config.
+    borderSize = 0;
+    progressColor = "over ${colors.normal.blue.hex}";
+    defaultTimeout = 1500;
+    width = 420;
   };
 }
