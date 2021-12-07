3870b8f24a456cd4307206f709e1ee7dbb8802e7
Luis Holanda <luiscmholanda@gmail.com>
2021-12-07 20:56:58 -0300

    feat: add doas/sudo config module

---
+++ modules/os-specific/linux/security.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+{
+  config = {
+    security.doas = {
+      enable = true;
+      extraRules = [{
+        users = [ config.user.name ];
+        persist = true;
+        # This is not thats secure, but don't having this is a PITA.
+        keepEnv = true;
+      }];
+    };
+
+    security.protectKernelImage = true;
+    security.rtkit.enable = true;
+    security.sudo.enable = !config.security.doas.enable;
+  };
+}
