53a8c50a71e8865bce88bc3d7cb07ee98b456ee6
Luis C. M. Holanda <luiscmholanda@gmail.com>
2022-07-08 22:58:47 -0300

    fix(intel): enable Intel compute runtime
    This should correctly enables GPU acceleration on Intel CPUs.
    
    Signed-off-by: Luis Holanda <luiscmholanda@gmail.com>

--- modules/os-specific/linux/intel.nix
+++ modules/os-specific/linux/intel.nix
@@ -12,6 +12,12 @@ in {
 
   config = mkIf (config.host.hardware.isIntel && isLinux) {
     hardware.cpu.intel.updateMicrocode = true;
-    hardware.opengl.extraPackages = with pkgs; [intel-media-driver vaapiIntel vaapiVdpau libvdpau-va-gl];
+    hardware.opengl.extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
   };
 }
