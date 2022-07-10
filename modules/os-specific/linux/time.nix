6656ed9866cfa4244fab912ea60f791bc8b4c59f
Luis C. M. Holanda <luiscmholanda@gmail.com>
2022-07-09 23:30:01 -0300

    feat(time): configure NTS synchronization
    This commit enables NTS synchronization via Chrony. It uses
    NTP.br servers and then Cloudflare as backup.
    
    Use chrony to provide better usage in laptop hosts.
    
    Signed-off-by: Luis Holanda <luiscmholanda@gmail.com>

--- modules/os-specific/linux/time.nix
+++ modules/os-specific/linux/time.nix
@@ -1,3 +1,19 @@
 {
-  config.services.localtime.enable = true;
+  config = {
+    services.chrony = {
+      enable = true;
+      enableNTS = true;
+    };
+    services.localtime.enable = true;
+    networking.timeServers = [
+      # Start with Brazil-hosted NTP servers
+      "a.st1.ntp.br"
+      "b.st1.ntp.br"
+      "c.st1.ntp.br"
+      "d.st1.ntp.br"
+      "gps.ntp.br"
+      # Use Cloudflare as backup
+      "time.cloudflare.com"
+    ];
+  };
 }
