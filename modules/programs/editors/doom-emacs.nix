cf7fc024caecaef52e5ada6dbc8577bb39c8fb9b
Luis Holanda <luiscmholanda@gmail.com>
2022-10-08 18:12:43 -0300

    doom-emacs: fix emacs wrapper after 29 upgrades
    For some reason that I didn't search on, the way that Emacs loads
    *.eln files changes at some point, breaking the previous way we
    created the wrapper.
    
    Fix this by linking the native-lisp directory into the wrapper output
    directory.

--- modules/programs/editors/doom-emacs.nix
+++ modules/programs/editors/doom-emacs.nix
@@ -2,33 +2,41 @@
   config,
   pkgs,
   lib,
-  inputs,
   ...
 }: let
   inherit (lib) mkEnableOption mkIf makeBinPath;
-  inherit (lib.my) wrapProgram;
   inherit (config.theme) fonts;
-  inherit (inputs) doom-emacs;
 
   emacsCfg = config.modules.editors.emacs;
   editorPkgs = config.modules.editors.extraPackages;
 
-  baseEmacs = pkgs.emacsPgtkNativeComp;
+  baseEmacs = pkgs.emacsPgtkNativeComp.overrideAttrs (_old: {
+    buildBuildInputs = [pkgs.gtk3];
+  });
 
   doomEmacsConfigSource = config.dotfiles.configDir + "/doom-emacs";
-  emacs = wrapProgram baseEmacs {
-    prefix.PATH = makeBinPath editorPkgs;
-    set = with fonts.family; {
-      DOOMDIR = doomEmacsConfigSource;
-      EMACSDIR = "${config.user.home.dir}/.config/emacs";
-      EMACS_MONO_FONT_FAMILY = monospace;
-      EMACS_VARIABLE_PITCH_FONT_FAMILY = sansSerif;
-      EMACS_SERIF_FONT_FAMILY = serif;
-      EMACS_UNICODE_FONT_FAMILY = "Latin Modern Math";
-      EMACS_TEXT_FONT_SIZE = fonts.size.text;
-      EMACS_UI_FONT_SIZE = fonts.size.ui;
-    };
-  };
+  emacs =
+    pkgs.runCommandLocal "doom-emacs" {
+      buildInputs = with pkgs; [makeBinaryWrapper];
+    } ''
+      mkdir -p $out/bin
+
+      for bin in $(find ${baseEmacs}/bin -not -name '.*' -a \( -type f -o -type l \) ); do
+        ln -s $(realpath $bin) $out/bin/$(basename $bin)
+        wrapProgram $out/bin/$(basename $bin) \
+            --prefix PATH : "${makeBinPath editorPkgs}" \
+            --set DOOMDIR ${doomEmacsConfigSource} \
+            --set EMACSDIR "${config.user.home.dir}/.config/emacs" \
+            --set EMACS_MONO_FONT_FAMILY "${fonts.family.monospace}" \
+            --set EMACS_VARIABLE_PITCH_FONT_FAMILY "${fonts.family.sansSerif}" \
+            --set EMACS_SERIF_FONT_FAMILY "${fonts.family.serif}" \
+            --set EMACS_UNICODE_FONT_FAMILY "Latin Modern Math" \
+            --set EMACS_TEXT_FONT_SIZE ${builtins.toString fonts.size.text} \
+            --set EMACS_UI_FONT_SIZE ${builtins.toString fonts.size.ui}
+      done
+
+      ln -s ${baseEmacs}/lib/emacs/*/native-lisp $out/native-lisp
+    '';
 
   doom = pkgs.writeScriptBin "doom" ''
     #!${pkgs.stdenv.shell}
@@ -102,8 +110,11 @@ in {
       nodePackages.bash-language-server
       nodePackages.vscode-json-languageserver
       nodePackages.typescript
+      nodePackages.typescript-language-server
       terraform-ls
       bazel-buildtools
+      rust-analyzer
+      pyright
     ];
 
     user.packages = [doom emacs];
