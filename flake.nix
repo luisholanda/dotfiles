c0b18f3e78b1d7fae13600cd99ab5999f88ea534
Luis C. M. Holanda <luiscmholanda@gmail.com>
2023-02-20 11:12:16 -0300

    nixpkgs: no need to use forked nixpkgs anymore

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Nix configurations.";
 
   inputs = {
-    nixpkgs.url = "github:luisholanda/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
