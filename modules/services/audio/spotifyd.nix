6b010402c242e5c077397667b3b736b765d9dc57
Luis C. M. Holanda <luiscmholanda@gmail.com>
2023-05-23 08:31:20 -0300

    spotifyd: use home-manager options

--- modules/services/audio/spotifyd.nix
+++ modules/services/audio/spotifyd.nix
@@ -6,56 +6,38 @@
   inherit (pkgs.lib) mkEnableOption mkIf;
   inherit (pkgs.stdenv) isDarwin;
   inherit (config.modules.services.audio) spotify;
-
-  spotifyd = pkgs.spotifyd.override {withMpris = true;};
-
-  configFile = (pkgs.formats.toml {}).generate "spotifyd.conf" {
-    global = {
-      username = "luiscmholanda";
-      password_cmd = "PASSWORD_STORE_DIR=~/.local/share/password-store ${pkgs.pass-wayland}/bin/pass spotify";
-      use_mpris = true;
-      dbus_type = "session";
-      backend =
-        if isDarwin
-        then "portaudio"
-        else "alsa";
-      device = "default";
-      volume_controller =
-        if isDarwin
-        then "softvol"
-        else "alsa";
-      device_name = "Spotifyd";
-      bitrate = 320;
-      cache_path = "${config.user.home.dir}/.cache/spotifyd";
-      initial_volume = "50";
-      volume_normalisation = true;
-      autoplay = true;
-      device_type = "computer";
-    };
-  };
-
-  mkIfEnabled = mkIf spotify.enable;
 in {
   options.modules.services.audio.spotify.enable = mkEnableOption "spotifyd";
 
   config = {
-    user.home.extraConfig.systemd.user.services.spotifyd = mkIfEnabled {
-      Unit = {
-        Description = "spotify daemon";
-        Documentation = "https://github.com/Spotifyd/spotifyd";
-      };
-
-      Install.WantedBy = ["default.target"];
-
-      Service = {
-        ExecStart = "${spotifyd}/bin/spotifyd --no-daemon --config-path ${configFile}";
-        Restart = "always";
-        RestartSec = 12;
+    user.home.services.spotifyd.enable = spotify.enable;
+    user.home.services.spotifyd.package = pkgs.spotifyd.override {withMpris = true;};
+    user.home.services.spotifyd.settings = {
+      global = {
+        username = "luiscmholanda";
+        password_cmd = "PASSWORD_STORE_DIR=~/.local/share/password-store ${pkgs.pass-wayland}/bin/pass spotify";
+        use_mpris = true;
+        dbus_type = "session";
+        backend =
+          if isDarwin
+          then "portaudio"
+          else "alsa";
+        device = "default";
+        volume_controller =
+          if isDarwin
+          then "softvol"
+          else "alsa";
+        device_name = "Spotifyd";
+        bitrate = 320;
+        cache_path = "${config.user.home.dir}/.cache/spotifyd";
+        initial_volume = "50";
+        volume_normalisation = true;
+        autoplay = true;
+        device_type = "computer";
       };
     };
-
     user.home.services.playerctld.enable = spotify.enable;
 
-    user.packages = mkIfEnabled [pkgs.spotify-tui];
+    user.packages = mkIf spotify.enable [pkgs.spotify-tui];
   };
 }
