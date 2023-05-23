6c085979110d1f008ccc5ab86814bf6e069a5330
Luis C. M. Holanda <luiscmholanda@gmail.com>
2023-05-23 08:30:28 -0300

    steam: add more dependencies to env and imporove gamemode stuff

--- modules/programs/games/steam.nix
+++ modules/programs/games/steam.nix
@@ -5,28 +5,51 @@
   ...
 }: let
   inherit (lib) mkIf mkEnableOption;
+  inherit (pkgs.stdenv) isLinux;
   inherit (builtins) fetchTarball;
 
   # TODO: move this to a overlay.
   proton-ge = let
     baseUrl = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download";
     name = "GE-Proton";
-    version = "7-53";
+    version = "8-3";
     releaseName = "${name}${version}";
   in
     fetchTarball {
       url = "${baseUrl}/${releaseName}/${releaseName}.tar.gz";
-      sha256 = "sha256:1cb0f7y4qfbpcg70fcmcrgd15xmpl18iljh5nw45c5rzvl6v0hm0";
+      sha256 = "sha256:0qzx52gz5y18mcvlxb26scr7x4jk4zshrziykhy069099w61ldjx";
     };
 in {
   options.modules.games.steam.enable = mkEnableOption "Steam";
 
   config = mkIf config.modules.games.steam.enable {
     programs.gamemode.enable = true;
+    programs.gamemode.settings = {
+      general = {
+        softrealtime = "auto";
+        renice = 18;
+      };
+      gpu.apply_gpu_optimisations = "accept-responsibility";
+    };
+    programs.gamescope.enable = true;
     programs.steam.enable = true;
 
     environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = proton-ge;
 
-    environment.systemPackages = with pkgs; [gamescope];
+    user.home.extraConfig.systemd.user.services.steam = mkIf isLinux rec {
+      Unit = {
+        Description = "A digital distribution platform";
+        After = ["graphical-session.target"];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.steam}/bin/steam -silent";
+        ExecStop = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+        Restart = "on-failure";
+        KillMode = "mixed";
+      };
+
+      Install.WantedBy = Unit.After;
+    };
   };
 }
--- overlays/applications/games/steam.nix
+++ overlays/applications/games/steam.nix
@@ -1,18 +1,21 @@
-(_final: prev: {
+(_: prev: {
   # Fix some proton issues in wayland.
   steam = prev.steam.override {
-    extraPkgs = pkgs:
+    extraLibraries = pkgs:
       with pkgs; [
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
+        libxcrypt
+        libmspack
         libpng
         libpulseaudio
         libvorbis
         stdenv.cc.cc.lib
         libkrb5
         keyutils
+        gamemode.lib
+        (pkgs.runCommandNoCCLocal "libxcrypt-1" {src = libxcrypt;} ''
+          mkdir -p $out/lib
+          cp $src/lib/libcrypt.so $out/lib/libcrypt.so.1
+        '')
       ];
   };
 })
